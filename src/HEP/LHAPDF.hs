287c23200b34acf1493fc02cf9a9eaa1371772ba
ianwookim <ianwookim@gmail.com>
2011-01-17 07:33:42 +0000

    add anti

--- src/HEP/LHAPDF.hs
+++ src/HEP/LHAPDF.hs
@@ -1,4 +1,5 @@
 module HEP.LHAPDF (
+  PartonType(..),
   initPDFSet,   
   alphasPDF, 
   numberPDF, 
@@ -12,6 +13,26 @@ import Foreign.C
 import Foreign.C.String
 import Foreign.Ptr
 import Foreign.ForeignPtr
+
+data PartonType = G | D | U | S | C | B | T 
+                | Dbar | Ubar | Sbar | Cbar | Bbar | Tbar
+
+
+anti :: PartonType -> PartonType 
+anti G = G 
+anti D = Dbar
+anti U = Ubar
+anti S = Sbar 
+anti C = Cbar 
+anti B = Bbar 
+anti T = Tbar 
+anti Dbar = D
+anti Ubar = U
+anti Sbar = S
+anti Cbar = C
+anti Bbar = B
+anti Tbar = T
+
 
 initPDFSet :: String -> IO () 
 initPDFSet name = do 
@@ -34,11 +55,27 @@ initPDF n = do
   let c_n = fromIntegral n 
   c_LHAPDF_initPDF c_n 
 
-xfx :: Double -> Double -> Int -> IO Double
-xfx x q n = do 
+partonType2Int :: PartonType -> Int
+partonType2Int G = 0 
+partonType2Int D = 1
+partonType2Int U = 2 
+partonType2Int S = 3 
+partonType2Int C = 4 
+partonType2Int B = 5 
+partonType2Int T = 6 
+partonType2Int Dbar = -1 
+partonType2Int Ubar = -2
+partonType2Int Sbar = -3
+partonType2Int Cbar = -4
+partonType2Int Bbar = -5
+partonType2Int Tbar = -6
+                   
+
+xfx :: Double -> Double -> PartonType -> IO Double
+xfx x q ptyp = do 
   let c_x = realToFrac x 
       c_q = realToFrac q
-      c_n = fromIntegral n 
+      c_n = (fromIntegral . partonType2Int) ptyp 
   c_xfx <- c_LHAPDF_xfx c_x c_q c_n
   return $ realToFrac c_xfx
   
